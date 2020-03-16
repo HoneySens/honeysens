8aa90af8414dde7a1efc21e124becb77ebc612ba
sylence <pascal.brueckner@sylence.cc>
2020-03-16 17:37:33 +0100

    Server: Support for the automatic removal of old backups

--- server/services/backup/backup.sh
+++ server/services/backup/backup.sh
@@ -4,6 +4,16 @@ set -e
 mysqldump -h "${DB_HOST}" -P "${DB_PORT}" -u "${DB_USER}" -p"${DB_PASSWORD}" "${DB_NAME}" >/srv/db.sql
 if [[ "${1}" == "-l" ]]; then
   TARGET=$(printf "/srv/backup/${CRON_TEMPLATE}.tar.bz2" "$(date +%Y%m%e-%H%M)")
+  # Remove old backups
+  if [[ -n "${CRON_KEEP}" && "${CRON_KEEP}" -gt 0 && $(find /srv/backup -name '*.tar.bz2' | wc -l) -gt 0 ]]; then
+    echo "Cleaning up old backups"
+    OIFS="$IFS"
+    IFS=$'\n'
+    for f in $(ls -t /srv/backup/*.tar.bz2 | tail -n +"${CRON_KEEP}"); do
+      rm -v "${f}"
+    done
+    IFS="$OIFS"
+  fi
   echo "Writing backup to ${TARGET}"
 else
   TARGET="-"
