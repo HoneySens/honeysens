9aac62032cd2cc7b663c68448fda23088437c7f4
sylence <pascal.brueckner@sylence.cc>
2020-04-25 11:44:50 +0200

    BBB firmware migrated to Debian buster

--- sensor/platforms/bbb/prepare.sh
+++ sensor/platforms/bbb/prepare.sh
@@ -7,20 +7,17 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 
 # Basic dependencies
-apt-get -y install macchanger resolvconf wpasupplicant
+apt-get -y install macchanger resolvconf tcpdump wpasupplicant
 
 # Install Docker CE (follows https://docs.docker.com/engine/installation/linux/docker-ce/debian)
-# TODO Infer the linux-header version from the currently installed linux-image packet (NOT the running kernel)
-apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common linux-headers-4.9.76-ti-r91
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
-echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-     $(lsb_release -cs) stable" | \
-    tee /etc/apt/sources.list.d/docker.list
+apt-get -y install apt-transport-https curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository "deb [arch=armhf] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce
 
 # Build sensor manager
-apt-get install -y python-pip libcurl4-gnutls-dev libgnutls28-dev cntlm
+apt-get install -y python-pip libcurl4-gnutls-dev libgnutls28-dev cntlm libyaml-dev libzmq3-dev
 mkdir /etc/manager
 cd /opt/manager
 python setup.py install
@@ -38,7 +35,10 @@ rm /etc/systemd/system/sysinit.target.wants/systemd-timesyncd.service
 # Disable the dnsmasq daemon that usually launches as part of the USB ethernet gadget, but listens on 0.0.0.0 and blocks port 53
 mv /etc/dnsmasq.d /etc/dnsmasq.d.disabled
 
-# Clean up apt cache
+# Disable background APT activity
+apt-get --purge remove -y unattended-upgrades
+
+# Clean up APT cache
 apt-get clean
 
 # Revision marker
