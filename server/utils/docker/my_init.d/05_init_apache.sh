b40513958bd10d6b9d21d79add7bfeaaea821061
sylence <pascal.brueckner@sylence.cc>
2020-08-03 12:06:26 +0200

    Added option to force TLS 1.2 for the REST-API/Web interface

--- server/utils/docker/my_init.d/05_init_apache.sh
+++ server/utils/docker/my_init.d/05_init_apache.sh
@@ -26,3 +26,14 @@ else
     sed -i -e "s|#*CustomLog.*|#CustomLog /dev/stdout combined|" /etc/apache2/sites-available/*.conf
     a2disconf other-vhosts-access-log
 fi
+
+# TLS Protocol configuration (either force TLS 1.2 or be a bit more liberal by default)
+if [[ "$TLS_FORCE_12" = "true" ]]; then
+    echo "Apache: Forcing TLS 1.2"
+    sed -i -e "s|SSLProtocol.*|SSLProtocol -All +TLSv1.2|" /etc/apache2/sites-available/*.conf
+else
+    # We currently disable TLSv1.3 because client cert auth with renegotiation is currently not supported in all browsers
+    # and leads to issues when attempting to download firmware directly from the web app.
+    # Reference: https://bugs.launchpad.net/ubuntu/+source/firefox/+bug/1834671
+    sed -i -e "s|SSLProtocol.*|SSLProtocol All -TLSv1.3 -SSLv2 -SSLv3|" /etc/apache2/sites-available/*.conf
+fi
