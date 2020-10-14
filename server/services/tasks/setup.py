e77f61881c9c627b73758d68eb9761c274c2b003
sylence <pascal.brueckner@sylence.cc>
2020-10-14 14:36:26 +0200

    Temporarily pin celery to v4.4.7 because of a bug in v5.0.0

--- server/services/tasks/setup.py
+++ server/services/tasks/setup.py
@@ -11,7 +11,7 @@ setup(
     license='Apache 2.0',
     packages=find_packages(),
     install_requires=[
-        'celery',
+        'celery==4.4.7',  # Until https://github.com/celery/celery/issues/6380 is fixed
         'defusedxml',
         'pymysql',
         'redis'
