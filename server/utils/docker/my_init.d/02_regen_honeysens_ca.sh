36eccf8778a092760958f363ca5be678a8c132b8
sylence <pascal.brueckner@sylence.cc>
2021-02-16 10:44:51 +0100

    Fix: OpenSSL subject parsing during CA+TLS certificate regeneration

--- server/utils/docker/my_init.d/02_regen_honeysens_ca.sh
+++ server/utils/docker/my_init.d/02_regen_honeysens_ca.sh
@@ -14,7 +14,7 @@ if [[ -e /opt/HoneySens/data/CA/cakey.pem ]] && [[ -e /opt/HoneySens/data/CA/cac
     mv -v /opt/HoneySens/data/CA/cacert.pem /opt/HoneySens/data/CA/ca.crt
     mv -v /opt/HoneySens/data/CA/openssl.cnf /opt/HoneySens/data/CA/openssl_ca.cnf
     # Re-sign the legacy CA cert with its original parameters so that it stays compatible with already issued client certs
-    CA_SUBJECT=$(openssl x509 -noout -subject -in /opt/HoneySens/data/CA/ca.crt | sed -e "s/subject=\(.*\)/\1/" | awk '{$1=$1};1')
+    CA_SUBJECT=$(openssl x509 -noout -subject -nameopt compat -in /opt/HoneySens/data/CA/ca.crt | sed -e "s/subject=\(.*\)/\1/" | awk '{$1=$1};1')
     echo "  Found CA subject: ${CA_SUBJECT}"
     openssl req -nodes -extensions v3_ca -new -x509 -key /opt/HoneySens/data/CA/ca.key -out /opt/HoneySens/data/CA/ca.crt -days ${CA_EXPIRATION_DAYS} -config /opt/HoneySens/data/CA/openssl_ca.cnf -subj "${CA_SUBJECT}" -passin pass:asdf
 fi
@@ -31,7 +31,7 @@ if [[ ! -e /opt/HoneySens/data/CA/ca.key ]]; then
 elif [[ "$FORCE" = "force" ]]; then
   # Use subject line of existing certificate
   if [[ -e /opt/HoneySens/data/CA/ca.crt ]]; then
-    CA_SUBJECT=$(openssl x509 -noout -subject -in /opt/HoneySens/data/CA/ca.crt | sed -e "s/subject=\(.*\)/\1/" | awk '{$1=$1};1')
+    CA_SUBJECT=$(openssl x509 -noout -subject -nameopt compat -in /opt/HoneySens/data/CA/ca.crt | sed -e "s/subject=\(.*\)/\1/" | awk '{$1=$1};1')
   fi
   echo "Generating new CA certificate for existing key using CA subject ${CA_SUBJECT}"
   openssl req -nodes -new -x509 -extensions v3_ca -key /opt/HoneySens/data/CA/ca.key -out /opt/HoneySens/data/CA/ca.crt -days ${CA_EXPIRATION_DAYS} -config /opt/HoneySens/data/CA/openssl_ca.cnf -subj "${CA_SUBJECT}"
