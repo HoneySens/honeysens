09776fd538bb52f5173236040c89fdce3eb81afb
sylence <pascal.brueckner@sylence.cc>
2021-09-02 11:20:31 +0200

    Backup: Added -y switch to mysqldump to prevent some warnings during backup

--- server/services/backup/backup.sh
+++ server/services/backup/backup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-mysqldump -h "${DB_HOST}" -P "${DB_PORT}" -u "${DB_USER}" -p"${DB_PASSWORD}" "${DB_NAME}" >/srv/db.sql
+mysqldump -h "${DB_HOST}" -P "${DB_PORT}" -u "${DB_USER}" -p"${DB_PASSWORD}" -y "${DB_NAME}" >/srv/db.sql
 if [[ "${1}" == "-l" ]]; then
   TARGET=$(printf "/srv/backup/${CRON_TEMPLATE}.tar.bz2" "$(date +%Y%m%d-%H%M)")
   # Remove old backups
