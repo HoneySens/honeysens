433e214fe290576590c8eb3eca4125bc96c6cafa
sylence <pascal.brueckner@sylence.cc>
2021-10-08 11:25:46 +0200

    Fix: Notification template max length increased

--- server/app/models/entities/TemplateOverlay.php
+++ server/app/models/entities/TemplateOverlay.php
@@ -24,7 +24,7 @@ class TemplateOverlay {
     /**
      * Overridden user-defined template content.
      *
-     * @Column(type="string")
+     * @Column(type="text")
      */
     protected $template;
 
