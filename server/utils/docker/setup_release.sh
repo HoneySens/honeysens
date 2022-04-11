6dc1fb59a3fa886666a17b84de17ab88d68edaeb
sylence <pascal.brueckner@sylence.cc>
2022-04-11 19:10:20 +0200

    Unprivileged server preparations: Volume access and permissions

--- server/utils/docker/setup_release.sh
+++ server/utils/docker/setup_release.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Apache
-chown -R www-data:www-data /opt/HoneySens/cache/ /opt/HoneySens/data/
+chown -R hs:hs /opt/HoneySens/cache/ /opt/HoneySens/data/
 mkdir -p /etc/apache2/conf
 cp -v /opt/HoneySens/utils/docker/apache.http.conf /etc/apache2/sites-available/honeysens_http.conf
 cp -v /opt/HoneySens/utils/docker/apache.ssl.conf /etc/apache2/sites-available/honeysens_ssl.conf
@@ -17,13 +17,13 @@ cp -v /opt/HoneySens/utils/docker/sudoers.conf /etc/sudoers.d/honeysens
 cp -v /opt/HoneySens/utils/docker/my_init.d/01_init_volumes.sh /etc/my_init.d/
 cp -v /opt/HoneySens/utils/docker/my_init.d/02_regen_honeysens_ca.sh /etc/my_init.d/
 cp -v /opt/HoneySens/utils/docker/my_init.d/03_regen_https_cert.sh /etc/my_init.d/
-cp -v /opt/HoneySens/utils/docker/my_init.d/04_fix_permissions.sh /etc/my_init.d/
 cp -v /opt/HoneySens/utils/docker/my_init.d/05_init_apache.sh /etc/my_init.d/
 cp -v /opt/HoneySens/utils/docker/my_init.d/06_update_deployment.py /etc/my_init.d/
 
 # Create templates from the data directory to allow reinitialization of empty volumes
 mkdir -p /opt/HoneySens/templates
 cp -var /opt/HoneySens/data /opt/HoneySens/templates/
+chown -R hs:hs /opt/HoneySens/templates
 
 # Services
 cp -vr /opt/HoneySens/utils/docker/services/apache2 /etc/service
