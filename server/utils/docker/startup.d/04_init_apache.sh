0fdc28ac0171ec46698c9303275df2473106d6c6
sylence <pascal.brueckner@sylence.cc>
2022-04-19 14:38:11 +0200

    Server: Enable TLS 1.3

--- server/utils/docker/startup.d/04_init_apache.sh
+++ server/utils/docker/startup.d/04_init_apache.sh
@@ -15,13 +15,10 @@ else
     a2disconf other-vhosts-access-log
 fi
 
-# TLS Protocol configuration (either force TLS 1.2 or be a bit more liberal by default)
+# TLS Protocol configuration (either force TLS 1.2+ or be a bit more liberal by default)
 if [[ "$TLS_FORCE_12" = "true" ]]; then
-    echo "Apache: Forcing TLS 1.2"
-    sed -i -e "s|SSLProtocol.*|SSLProtocol -All +TLSv1.2|" /etc/apache2/sites-available/*.conf
+    echo "Apache: Forcing TLS 1.2+"
+    sed -i -e "s|SSLProtocol.*|SSLProtocol -All +TLSv1.2 +TLSv1.3|" /etc/apache2/sites-available/*.conf
 else
-    # We currently disable TLSv1.3 because client cert auth with renegotiation is currently not supported in all browsers
-    # and leads to issues when attempting to download firmware directly from the web app.
-    # Reference: https://bugs.launchpad.net/ubuntu/+source/firefox/+bug/1834671
-    sed -i -e "s|SSLProtocol.*|SSLProtocol All -TLSv1.3 -SSLv2 -SSLv3|" /etc/apache2/sites-available/*.conf
+    sed -i -e "s|SSLProtocol.*|SSLProtocol All -SSLv2 -SSLv3|" /etc/apache2/sites-available/*.conf
 fi
