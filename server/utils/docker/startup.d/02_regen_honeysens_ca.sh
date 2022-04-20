52edaf7b55a92705336e587c77a4617657a950e2
sylence <pascal.brueckner@sylence.cc>
2022-04-20 14:32:50 +0200

    Server: Cleanup of update instructions and version-specific code from prior 2.4.0

--- server/utils/docker/startup.d/02_regen_honeysens_ca.sh
+++ server/utils/docker/startup.d/02_regen_honeysens_ca.sh
@@ -7,18 +7,6 @@ CA_EXPIRATION_DAYS=365
 # If "force" is given as additional parameter, certificate generation is forced
 FORCE=${1:-no}
 
-# Server versions < 0.9.0 were using /opt/HoneySens/data/CA/cakey.pem and /opt/HoneySens/data/CA/cacert.pem
-if [[ -e /opt/HoneySens/data/CA/cakey.pem ]] && [[ -e /opt/HoneySens/data/CA/cacert.pem ]]; then
-    echo "Importing CA from server < 0.9.0"
-    mv -v /opt/HoneySens/data/CA/cakey.pem /opt/HoneySens/data/CA/ca.key
-    mv -v /opt/HoneySens/data/CA/cacert.pem /opt/HoneySens/data/CA/ca.crt
-    mv -v /opt/HoneySens/data/CA/openssl.cnf /opt/HoneySens/data/CA/openssl_ca.cnf
-    # Re-sign the legacy CA cert with its original parameters so that it stays compatible with already issued client certs
-    CA_SUBJECT=$(openssl x509 -noout -subject -nameopt compat -in /opt/HoneySens/data/CA/ca.crt | sed -e "s/subject=\(.*\)/\1/" | awk '{$1=$1};1')
-    echo "  Found CA subject: ${CA_SUBJECT}"
-    openssl req -nodes -extensions v3_ca -new -x509 -key /opt/HoneySens/data/CA/ca.key -out /opt/HoneySens/data/CA/ca.crt -days ${CA_EXPIRATION_DAYS} -config /opt/HoneySens/data/CA/openssl_ca.cnf -subj "${CA_SUBJECT}" -passin pass:asdf
-fi
-
 # Update OpenSSL CA config
 if [[ -e /opt/HoneySens/templates/data/CA/openssl_ca.cnf ]]; then
     cp -va /opt/HoneySens/templates/data/CA/openssl_ca.cnf /opt/HoneySens/data/CA/
