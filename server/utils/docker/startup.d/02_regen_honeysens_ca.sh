641e1e1dbc677176a8a92b2917edf386dc91da87
sylence <pascal.brueckner@sylence.cc>
2022-04-21 19:59:56 +0200

    Server: Recreate self-signed TLS certificate chain in case the existing CA key size is invalid (not 2048 bit)

--- server/utils/docker/startup.d/02_regen_honeysens_ca.sh
+++ server/utils/docker/startup.d/02_regen_honeysens_ca.sh
@@ -12,6 +12,18 @@ if [[ -e /opt/HoneySens/templates/data/CA/openssl_ca.cnf ]]; then
     cp -va /opt/HoneySens/templates/data/CA/openssl_ca.cnf /opt/HoneySens/data/CA/
 fi
 
+# Check existing CA key size, remove old key/certs if invalid
+if [[ -e /opt/HoneySens/data/CA/ca.key ]]; then
+  CA_KEY_SIZE=$(openssl rsa -in /opt/HoneySens/data/CA/ca.key -text -noout | grep "Private-Key")
+  if [[ ! "$CA_KEY_SIZE" =~ "2048 bit" ]]; then
+    echo "Size of existing CA key too small, recreating certificate chain"
+    rm -vf /opt/HoneySens/data/CA/ca.key /opt/HoneySens/data/CA/ca.crt /opt/HoneySens/data/CA/ca.srl /opt/HoneySens/data/https.chain.crt /opt/HoneySens/data/https.crt /opt/HoneySens/data/https.csr /opt/HoneySens/data/https.key
+    if [[ ! -s /srv/tls/server.crt && ! -s /srv/tls/server.key ]]; then
+      echo "Self-signed TLS certificates are active, ALL sensors need to be reinstalled after this update!"
+    fi
+  fi
+fi
+
 # Generate a new CA if none was found
 if [[ ! -e /opt/HoneySens/data/CA/ca.key ]]; then
   echo "Generating new CA key and certificate with subject ${CA_SUBJECT}"
