edaa687a3f6ded2f75bfc7f3210c5dbedb5faf02
sylence <pascal.brueckner@sylence.cc>
2022-04-21 09:22:01 +0200

    Fix: Foreign UID on data volume check stops the container in case of failure

--- server/utils/docker/startup.d/01_init_volumes.sh
+++ server/utils/docker/startup.d/01_init_volumes.sh
@@ -3,7 +3,7 @@ set -e
 
 # Refuse startup if files that don't belong to uid 1000 are found on the data volume (indicates a version <= 2.3.0)
 if find /opt/HoneySens/data ! -uid 1000 | grep . >/dev/null 2>&1; then
-  echo "Error: Files not owned by uid 1000 found on data volume!\nIf these volumes belong to a server running revision 2.3.0 or earlier, please update to revision 2.4.0 first."
+  echo -e 'Error: Files not owned by uid 1000 found on data volume!\nIf these volumes belong to a server running revision 2.3.0 or earlier, please update to revision 2.4.0 first.'
   exit 1
 fi
 
