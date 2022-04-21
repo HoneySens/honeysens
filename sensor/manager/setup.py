f3280f8667fc35b6b4be3c9b27fb853d48e1e11d
sylence <pascal.brueckner@sylence.cc>
2022-04-21 19:54:19 +0200

    BBB and Docker firmware versions pushed to 2.5.0

--- sensor/manager/setup.py
+++ sensor/manager/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='honeysens-manager',
-    version='2.4.0',
+    version='2.5.0',
     description='HoneySens sensor management daemon',
     author='Pascal Brueckner',
     author_email='pascal.brueckner@sylence.cc',
