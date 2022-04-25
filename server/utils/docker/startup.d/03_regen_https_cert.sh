50aadfb46167d657755e365daf1790b40a7e99b4
sylence <pascal.brueckner@sylence.cc>
2022-04-25 15:59:31 +0200

    Server: Ability to mount TLS cert/key pair in separate subdirectory /srv/tls/server/

--- server/utils/docker/startup.d/03_regen_https_cert.sh
+++ server/utils/docker/startup.d/03_regen_https_cert.sh
@@ -15,8 +15,11 @@ FORCE=${1:-no}
 #   /srv/tls/https.key -> /srv/tls/server.key
 # for a custom certificate/key pair mounted at /srv/tls/server.crt and /srv/tls/server.key or JUST
 #   /srv/tls/https.crt -> /srv/tls/server.crt
-# for a custom certificate pair mounted at /srv/tls/server.crt without a key, e.g. when running just the plain HTTP API
-# behind a TLS proxy, in which case we just require the certificate for distribution to sensors.
+# for a custom certificate mounted at /srv/tls/server.crt without a key, e.g. when running just the plain HTTP API
+# behind a TLS proxy, in which case we just require the certificate for distribution to sensors or
+#   /srv/tls/https.crt -> /srv/tls/server/tls.crt
+#   /srv/tls/https.key -> /srv/tls/server/tls.key
+# for a custom certificate/key pair mounted inside /srv/tls/server/
 rm -f /srv/tls/https.crt /srv/tls/https.key  # Remove links from last execution
 if [[ -s /srv/tls/server.crt && -s /srv/tls/server.key ]]; then
   # Mounted cert/key pair
@@ -27,6 +30,11 @@ elif [[ -s /srv/tls/server.crt && ! -s /srv/tls/server.key ]]; then
   # Mounted cert without key
   echo "Using mounted TLS certificate without key"
   ln -s /srv/tls/server.crt /srv/tls/https.crt
+elif [[ -d /srv/tls/server && -s /srv/tls/server/tls.crt && -s /srv/tls/server/tls.key ]]; then
+  # Mounted cert/key pair inside directory
+  echo "Using TLS cert/key pair mounted inside subdirectory"
+  ln -s /srv/tls/server/tls.crt /srv/tls/https.crt
+  ln -s /srv/tls/server/tls.key /srv/tls/https.key
 else
   # Self-signed cert/key pair
   echo "Using self-signed TLS cert/key pair"
