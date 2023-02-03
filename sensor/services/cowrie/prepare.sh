1efc39563b0e24678c6ec21f795c0f1b61874c0b
sylence <pascal.brueckner@sylence.cc>
2023-02-03 16:27:16 +0100

    Service cowrie: Bumped to version 2.6.0 based on upstream version 2.4.0

--- sensor/services/cowrie/prepare.sh
+++ sensor/services/cowrie/prepare.sh
@@ -2,22 +2,22 @@
 set -e
 
 # Install requirements
-apk --update --no-cache add --virtual build-dependencies gcc g++ git curl py-virtualenv mpfr-dev mpc1-dev gmp-dev musl-dev python2-dev
-apk --update add bash openssh-keygen openssh-client openssl-dev libffi-dev py-pip zeromq-dev
+apk --update --no-cache add --virtual build-dependencies cargo curl gcc g++ libffi-dev openssl-dev python3-dev rust
+apk --update add bash py3-pip py3-pyzmq
 
 # Install cowrie from git
-curl -s -L https://github.com/cowrie/cowrie/archive/1.6.0.tar.gz -o /root/cowrie.tar.gz
+curl -s -L https://github.com/cowrie/cowrie/archive/refs/tags/v2.4.0.tar.gz -o /root/cowrie.tar.gz
 tar -xzf /root/cowrie.tar.gz -C /opt
-mv -v /root/honeysens.py /opt/cowrie-1.6.0/src/cowrie/output/
-mv -v /root/cowrie.cfg /opt/cowrie-1.6.0/etc/
+sed -i "/packaging/d" /opt/cowrie-2.4.0/requirements.txt  # use system "packaging" package
+mv -v /root/honeysens.py /opt/cowrie-2.4.0/src/cowrie/output/
+mv -v /root/cowrie.cfg /opt/cowrie-2.4.0/etc/
 mv -v /root/run.sh /opt/
-(cd /opt/cowrie-1.6.0 && patch -p1 -i /root/dont_daemonize.patch)
-pip install --no-cache-dir -r /opt/cowrie-1.6.0/requirements.txt bcrypt pyzmq
+pip3 install --no-cache-dir -r /opt/cowrie-2.4.0/requirements.txt
 
 # Setup permissions
 adduser -D -s /bin/sh cowrie cowrie
-chmod -R 777 /opt/cowrie-1.6.0/var
+chmod -R 777 /opt/cowrie-2.4.0/var
 
 # Clean up
 apk del build-dependencies
-rm -rf /var/cache/apk/* /root/*
+rm -rf /var/cache/apk/* /root/* /root/.cargo /root/.cache
--- sensor/services/cowrie/run.sh
+++ sensor/services/cowrie/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
+export COWRIE_STDOUT=yes
 
 # Remove potential stale PID file, otherwise twistd might not start
-rm -f /opt/cowrie-1.6.0/var/run/cowrie.pid
+rm -f /opt/cowrie-2.4.0/var/run/cowrie.pid
 
-exec /opt/cowrie-1.6.0/bin/cowrie start
+exec /opt/cowrie-2.4.0/bin/cowrie start
