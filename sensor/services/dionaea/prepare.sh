a8e664e0f3ef9baa14650ed84d13c94213dfc2b8
sylence <pascal.brueckner@sylence.cc>
2023-09-26 16:23:27 +0200

    Service dionaea: Bumped to version 2.7.0 based on official Ubuntu 22.04 images
    Including a bugfix in the reporting module

--- sensor/services/dionaea/prepare.sh
+++ sensor/services/dionaea/prepare.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env sh
 set -e
 
-apt-get update
-apt-get install -y git build-essential cmake check cython3 libcurl4-openssl-dev libemu-dev libev-dev libglib2.0-dev libloudmouth1-dev libnetfilter-queue-dev libnl-3-dev libnl-route-3-dev libpcap-dev libssl-dev libtool libudns-dev python3 python3-dev python3-bson python3-yaml fonts-liberation python3-zmq
+apt update
+apt install -y git build-essential cmake check curl cython3 libcurl4-openssl-dev libev-dev libglib2.0-dev libloudmouth1-dev libnetfilter-queue-dev libnl-3-dev libnl-route-3-dev libpcap-dev libssl-dev libtool libudns-dev python3 python3-dev python3-bson python3-yaml fonts-liberation python3-zmq
 
+# Install libemu from source
+curl -s -L https://github.com/buffer/libemu/archive/refs/tags/v1.0.5.tar.gz -o /root/libemu.tar.gz
+tar -xzf /root/libemu.tar.gz -C /root
+autoreconf -v -i /root/libemu-1.0.5/
+(cd /root/libemu-1.0.5; ./configure && make install)
+
+# Install dionaea from source
 git clone https://github.com/DinoTools/dionaea.git /root/dionaea
 (cd /root/dionaea; git checkout 4e459f1b672a5b4c1e8335c0bff1b93738019215)
 
@@ -42,7 +49,7 @@ patch -d /opt/dionaea -p1 < /root/config.patch
 ln -vs ../ihandlers-available/log_honeysens.yaml /opt/dionaea/etc/dionaea/ihandlers-enabled/log_honeysens.yaml
 
 # Cleanup
-apt-get remove -y build-essential cmake git check
-apt-get autoremove -y
-apt-get remove -y "*-dev"
-rm -r /var/cache/apt/* /root/dionaea
+apt remove -y build-essential cmake curl git check
+apt autoremove -y
+apt remove -y "*-dev"
+rm -r /var/cache/apt/* /root/dionaea /root/libemu-1.0.5 /root/libemu.tar.gz
