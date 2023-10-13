798c6e2aa7489542ae0196ad8df1b935bf18a477
sylence <pascal.brueckner@sylence.cc>
2023-10-13 11:55:13 +0200

    Service glastopf: Bumped to version 2.7.0 based on latest git revision
    Remaining unused TLS fragments removed

--- sensor/services/glastopf/run.sh
+++ sensor/services/glastopf/run.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env sh
 
-# Generate random CN
-#CN=`pwgen 12 1`
-
 mkdir -p /tmp/glastopf
 cp /etc/glastopf/glastopf.cfg /tmp/glastopf/
-#openssl req -new -x509 -days 3650 -nodes -config /opt/openssl.conf -out /tmp/glastopf/stunnel.pem -keyout /tmp/glastopf/stunnel.pem -subj "/CN=$CN"
 cd /tmp/glastopf
-#/usr/bin/stunnel /opt/stunnel.conf &
 glastopf-runner
