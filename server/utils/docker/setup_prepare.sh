179c6eaa53a3402dca054bf45bd74e2ac5a93217
sylence <pascal.brueckner@sylence.cc>
2024-03-28 17:59:43 +0100

    Backend PHP8 migration

--- server/utils/docker/setup_prepare.sh
+++ server/utils/docker/setup_prepare.sh
@@ -11,10 +11,15 @@ groupadd -g 1000 hs
 useradd -m -u 1000 -g 1000 hs
 
 # Basic dependencies
-apt-get install -y software-properties-common screen python3-openssl python3-pymysql curl openssl apache2 vim iproute2 netcat php7.4 php7.4-mbstring php7.4-mysql php7.4-xml php7.4-ldap
+apt-get install -y software-properties-common screen python3-openssl python3-pymysql curl openssl apache2 vim iproute2 netcat
+
+# PHP 8
+add-apt-repository -y ppa:ondrej/php
+apt-get -qq update
+apt-get install -y php8.3 php8.3-mbstring php8.3-mysql php8.3-xml php8.3-ldap
 
 # Apache
-sed -i -e 's/upload_max_filesize.*/upload_max_filesize = 100M/' -e 's/post_max_size.*/post_max_size = 100M/' /etc/php/7.4/apache2/php.ini
+sed -i -e 's/upload_max_filesize.*/upload_max_filesize = 100M/' -e 's/post_max_size.*/post_max_size = 100M/' /etc/php/8.3/apache2/php.ini
 sed -i -e 's/ServerTokens OS/ServerTokens Prod/' /etc/apache2/conf-enabled/security.conf
 a2enmod rewrite ssl headers proxy_http
 a2dissite 000-default
