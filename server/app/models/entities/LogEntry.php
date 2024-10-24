b9f1a1c2ddc8c97d9df61cc21ed790faa7d265f2
sylence <pascal.brueckner@sylence.cc>
2024-10-24 15:52:23 +0200

    Refactoring log entry entity

--- server/app/models/entities/LogEntry.php
+++ server/app/models/entities/LogEntry.php
@@ -20,146 +20,48 @@ class LogEntry {
     #[Id]
     #[Column(type: Types::INTEGER)]
     #[GeneratedValue]
-    protected $id;
+    private int $id;
 
     #[Column(type: Types::DATETIME_MUTABLE)]
-    protected $timestamp;
+    public \DateTime $timestamp;
 
     /**
      * ID of the user that performed this action.
      */
     #[Column(type: Types::INTEGER, nullable: true)]
-    protected $userID;
+    public ?int $userID;
 
     /**
-     * ID of the resource that was subject to this action.
+     * ID of the resource that was subject of this action.
      * If a resource with this ID doesn't exist anymore, this log entry references historic data.
      */
     #[Column(type: Types::INTEGER, nullable: true)]
-    protected $resourceID;
+    public ?int $resourceID;
 
     /**
-     * The type of resource that was subject to this action.
+     * The type of resource that was subject of this action.
      */
-    #[Column(type: Types::INTEGER)]
-    protected $resourceType = LogResource::GENERIC->value;
+    #[Column()]
+    public LogResource $resourceType = LogResource::GENERIC;
 
     /**
      * The actual log message, e.g. what happened.
      */
     #[Column(type: Types::STRING)]
-    protected $message;
+    public string $message;
 
-    /**
-     * @return integer
-     */
-    public function getId() {
+    public function getId(): int {
         return $this->id;
-    }
-
-    /**
-     * Set timestamp
-     *
-     * @param \DateTime $timestamp
-     * @return LogEntry
-     */
-    public function setTimestamp(\DateTime $timestamp) {
-        $this->timestamp = $timestamp;
-        return $this;
-    }
-
-    /**
-     * Get timestamp
-     *
-     * @return \DateTime
-     */
-    public function getTimestamp() {
-        return $this->timestamp;
-    }
-
-    /**
-     * Sets the user ID this log entry can be attributed to.
-     *
-     * @param int|null $userID
-     * @return LogEntry
-     */
-    public function setUserID($userID = null) {
-        $this->userID = $userID;
-        return $this;
-    }
-
-    /**
-     * Returns the user ID this log entry can be attributed to.
-     *
-     * @return int|null
-     */
-    public function getUserID() {
-        return $this->userID;
-    }
-
-    /**
-     * Sets the resource ID this log entry can be attributed to.
-     *
-     * @param int|null $resourceID
-     * @return LogEntry
-     */
-    public function setResourceID($resourceID = null) {
-        $this->resourceID = $resourceID;
-        return $this;
-    }
-
-    /**
-     * Returns the resource ID this log entry can be attributed to.
-     *
-     * @return int|null
-     */
-    public function getResourceID() {
-        return $this->resourceID;
-    }
-
-    /**
-     * Sets the resource type this log entry can be attributed to.
-     */
-    public function setResourceType(LogResource $resourceType): LogEntry {
-        $this->resourceType = $resourceType->value;
-        return $this;
-    }
-
-    /**
-     * Returns the resource type this log entry can be attributed to.
-     */
-    public function getResourceType(): LogResource {
-        return LogResource::from($this->resourceType);
-    }
-
-    /**
-     * Sets the message content of this log entry.
-     *
-     * @param string $message
-     * @return LogEntry
-     */
-    public function setMessage($message) {
-        $this->message = $message;
-        return $this;
-    }
-
-    /**
-     * Returns the message content of this log entry.
-     *
-     * @return string
-     */
-    public function getMessage() {
-        return $this->message;
     }
 
     public function getState() {
         return array(
             'id' => $this->getId(),
-            'timestamp' => $this->getTimestamp()->format('U'),
-            'user_id' => $this->getUserID(),
-            'resource_id' => $this->getResourceID(),
-            'resource_type' => $this->getResourceType(),
-            'message' => $this->getMessage()
+            'timestamp' => $this->timestamp->format('U'),
+            'user_id' => $this->userID,
+            'resource_id' => $this->resourceID,
+            'resource_type' => $this->resourceType->value,
+            'message' => $this->message
         );
     }
 }
