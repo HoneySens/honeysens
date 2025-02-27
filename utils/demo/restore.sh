b919ff6570defc50dea80660b3f9e83419942e9d
sylence <pascal.brueckner@sylence.cc>
2025-02-27 18:02:18 +0100

    Add demo environment creation scripts

---
+++ utils/demo/restore.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Performing restoration from backup archive"
+echo "  Verifying service status"
+
+if nc -z server 443 2>/dev/null; then
+    echo "  Can't perform restoration: API service is reachable, please shut it down first"
+    exit 1
+else
+    echo "  API: down (OK)"
+fi
+
+if nc -z "${HS_REGISTRY_HOST}" "${HS_REGISTRY_PORT}" 2>/dev/null; then
+    echo "  Can't perform restoration: Registry service is reachable, please shut it down first"
+    exit 1
+else
+    echo "  Registry: down (OK)"
+fi
+
+echo "  Waiting for database"
+until mysql -h "${HS_DB_HOST}" -P "${HS_DB_PORT}" -u "${HS_DB_USER}" -p"${HS_DB_PASSWORD}" "${HS_DB_NAME}" -e '\q' >/dev/null 2>&1; do
+    sleep 1
+done
+echo "  Database: up (OK)"
+
+echo "  Extracting archive"
+mkdir -p /tmp/restore
+tar -xjpf ${1} -C /tmp/restore
+
+echo "  Verifying archive contents"
+if [[ ! -f /tmp/restore/db.sql ]]; then
+    echo "  Can't perform restoration: The backup archive is incomplete"
+    exit 1
+fi
+
+if [[ -d /tmp/restore/data && -d /tmp/restore/registry ]]; then
+  echo "  Importing volumes"
+  rm -rf /srv/data/* /srv/registry/*
+  mv -f /tmp/restore/data/* /srv/data/ >/dev/null 2>&1 || true
+  mv -f /tmp/restore/registry/* /srv/registry/ >/dev/null 2>&1 || true
+fi
+
+echo "  Importing database"
+echo "DROP DATABASE ${HS_DB_NAME}; CREATE DATABASE ${HS_DB_NAME}" | mysql -h "${HS_DB_HOST}" -P "${HS_DB_PORT}" -u "${HS_DB_USER}" -p"${HS_DB_PASSWORD}" "${HS_DB_NAME}"
+mysql -h "${HS_DB_HOST}" -P "${HS_DB_PORT}" -u "${HS_DB_USER}" -p"${HS_DB_PASSWORD}" "${HS_DB_NAME}" </tmp/restore/db.sql
+
+echo "  Importing sensor config"
+rm -rf /srv/sensor/*
+cp ${2} /srv/sensor/
+
+echo "  Cleaning up"
+rm -rf /tmp/restore
+
+echo "Restoration completed."
